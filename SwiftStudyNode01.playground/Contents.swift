e727e3f173c3d080c74b85975220ca52c8b9c05a
yanglei <:39:39yanglei3kyou@126.com>
2015-09-17 11:14:26 +0800

    150917A
    Swift Begin

---
+++ SwiftStudyNode01.playground/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// The Swift Programming Language (Swift 2)
+// Page 5
+print("Hello, world!")
+
+// Page 6
+let constValue: Int
+constValue = 1
+
+var varValue = 1
+varValue = 2
+
+// Page 7
+let experiment7Value: Float = 4
+
+
+let label7 = "The width is "
+let width7 = 94
+let widthLabel7 = label7 + String(width7)
+
+
+// Page 8
+let ageValue8 = 90
+let ageAnther8 = 100
+let greetString8 = "Hello, I am \(ageAnther8 - ageValue8) years old"
+
+
+// Page 9
+var shoppingList = ["dog","fish","Chicken"]
+shoppingList[0] = "Big dog"
+shoppingList.count
+shoppingList.append("Small Cat")
+shoppingList.count
+print(shoppingList)
+
+
+var occupations: Dictionary = [
+    "key1":"value1",
+    "key2":"value2",
+]
+
+occupations["key13"] = "d"
+print(occupations)
+
+let emptyArray = [String]()
+let emptyDictionary = [String: Int]()
+
+
+
+
+
+
+
+
+
+
+
+
+

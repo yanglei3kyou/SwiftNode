cd16df735a5e8cda0b658740bdab72672524acbf
yanglei <:39:39yanglei3kyou@126.com>
2015-09-19 10:13:15 +0800

    150919A
    Add lazy init

--- SwiftStudyNode02.playground/Contents.swift
+++ SwiftStudyNode02.playground/Contents.swift
@@ -92,6 +92,37 @@ var errorChildren: iosParent = iosChildren(score: 98.0)
 errorChildren.greetString()
 
 
+// Page 28 懒加载
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength:Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+    
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
+
 
 
 

e91bcb658c431cef597fd19a218ae3994a3c03f3
yanglei <:39:39yanglei3kyou@126.com>
2015-09-20 15:33:19 +0800

    150920A
    Something about protocol and extension

--- SwiftStudyNode03.playground/Contents.swift
+++ SwiftStudyNode03.playground/Contents.swift
@@ -38,3 +38,73 @@ let king = Rank.King
 
 queen.biggerThanAnotherValue(jack)
 queen.biggerThanAnotherValue(king)
+
+// Use the init?(rawValue:) initializer 用行值来初始化枚举值
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+}
+
+// Another didn't have any meaning Enumerations
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription()->String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+    
+    func color()->String {
+        switch self {
+        case .Spades, .Clubs:
+            return "black"
+        case .Hearts, .Diamonds:
+            return "red"
+        }
+    }
+    
+}
+
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
+let heartsColor = hearts.color()
+
+// Struct
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription()->String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+let threeOfSpades = Card(rank: .Three, suit: .Spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+    case Half(String)
+}
+
+let success = ServerResponse.Result("6:00 am", "8:09 pm")
+let half = ServerResponse.Half("We should eat launch")
+let failure = ServerResponse.Error("Out of cheese")
+
+switch half {
+case let .Result(sunrise, sunset):
+    let serverResponese = "Sunrise is at \(sunrise) and sunset is at \(sunset)"
+case let .Error(error):
+    let serverResponse = "Failure ...\(error)"
+case let .Half(half):
+    let serverResponse = "Half \(half)"
+}
+
+
+
+
---
+++ SwiftStudyNode04.playground/Contents.swift
@@ -0,0 +1,71 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// The Swift Programming Language(Swift 2)
+// Protocols and Extensions
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class"
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjust."
+    }
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+
+// Question Yanglei about enumeration init
+//
+//enum SimpleEnumeration: ExampleProtocol {
+//    case Ace
+//    var simpleDescription:String {
+//        get {
+//            return self.simpleDescription
+//        }
+//        set {
+//            self.simpleDescription = newValue
+//        }
+//    }
+//    
+//    mutating func adjust() {
+//        simpleDescription += " enumeration (adjusted)"
+//    }
+//}
+//
+//var c:SimpleEnumeration = SimpleEnumeration.Ace
+//c.adjust()
+//c.simpleDescription
+
+// extension
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+print(7.simpleDescription)
+
+
+

6e3abe33da32f4de20c1ba2fa0e484458550f63f
yanglei <:39:39yanglei3kyou@126.com>
2015-09-21 16:49:25 +0800

    150921B
    Add something about  The Basic knowledge

---
+++ SwiftStudyNode05.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// The Swift Programming Language(Swift 2)
+// Lanuage Guide
+// The Basic
+// Page 49
+/*
+    Nonetheless = Nevertheless 尽管如此
+    extensive 大范围的;广泛的
+    throughout 遍及
+    expressive 有特殊意味的
+    whereas 然而
+    colon 冒号
+*/
+
+
+let maximumNumberOfLoginAttemps = 10
+var currentLoginAttemp = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+// Type Annotations 类型注解
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.1415029
+let 你好 = "你好世界"
+let 🐶🐂 = "Dogcow"
+
+
+
+
+

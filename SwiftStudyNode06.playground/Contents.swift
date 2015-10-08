2f6aba2048a0c6194279d03a365360392d97169d
yanglei <:39:39yanglei3kyou@126.com>
2015-10-08 15:36:56 +0800

    151008A
    Add something about Numeric Conversation

--- SwiftStudyNode06.playground/Contents.swift
+++ SwiftStudyNode06.playground/Contents.swift
@@ -8,6 +8,10 @@ import UIKit
 /*
     deduce 推断
     literal 字面的
+    exponent 指数
+    decimal 小数
+    underscores 下划线
+    optimization 优化
 */
 
 let meaningOfLife = 42
@@ -31,4 +35,45 @@ let decimalInteger = 17
 let binaryInteger = 0b10001
 let octalInteger = 0o21
 let hexadecimalInteger = 0x11
+
+/*
+    1.25e2 means 1.25×10², or 125.0
+*/
+/*
+    0xFp2 means 15×2²,or 60.0
+*/
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0     //Int((12+3/16)*2^0)
+let testHexadecimalDouble = (12+3/16.0)*1
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+
+// Numeric Type Conversion
+//let cannotBeNegative: UInt8 = -1
+//let tooBig: Int8 = Int8.max + 1
+
+// 数字计算的时候必须类型必须统一
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+//let twoThousandAddOne = twoThousand + one     This is Error
+let twoThousandAddOne = twoThousand + UInt16(one)
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let threeAddPi = Double(three) + pointOneFourOneFiveNine
+
+// Float(Double)->Int or Intger must be shorted!
+let integerPi = Int(pi)
+
+
+
+
+
+
+
 

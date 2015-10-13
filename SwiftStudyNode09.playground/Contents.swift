adedaa741655332f6b37e2406b6b2cd09f894b8f
yanglei <:39:39yanglei3kyou@126.com>
2015-10-13 14:42:22 +0800

    151013A

--- SwiftStudyNode09.playground/Contents.swift
+++ SwiftStudyNode09.playground/Contents.swift
@@ -8,6 +8,8 @@ import UIKit
     shorthand 速记，简写
     syntax 句法，语法，写法
     compatible 合适的
+    equivalent 相等物
+    comma 逗号
 */
 let testArray = [1,2,3,4]
 
@@ -62,5 +64,144 @@ for item in shoppingList {
 for (index, value) in shoppingList.enumerate() {
     print("Item \(index + 1):\(value)")
 }
+
+// Sets 
+// Creating and Initializing an Empty Set
+var letters = Set<Character>()
+print("letters if of type Set<Character> with \(letters.count) items.")
+
+letters.insert("a")
+letters = []
+
+// Creating a Set with an Array Literal
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+//var favoriteGenres: Set = ["Rock", "Classical", "Hip hop"]
+
+//Accessing and Modifying a Set
+print("I have \(favoriteGenres.count) favorite music genres.")
+
+if favoriteGenres.isEmpty {
+    print("As far as music goes, I'm not picky.")
+} else {
+    print("I have particular music preferences.")
+}
+
+favoriteGenres.insert("Jazz")
+
+if let removedGenre = favoriteGenres.remove("Rock") {
+    print("\(removedGenre)? I'm over it.")
+} else {
+    print("I never much cared for that.")
+}
+
+if favoriteGenres.contains("Funk") {
+    print("I get up on the good foot.")
+} else {
+    print("It's too funky in here.")
+}
+
+// Iterating Over a Set
+for genre in favoriteGenres {
+    print("\(genre)")
+}
+
+for genre in favoriteGenres.sort() {
+    print("\(genre)")
+}
+
+// Performing Set Operations
+// Fundamental Set Operations
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
+let singledigitPrimeNumbers: Set = [2, 3, 5, 7]
+
+oddDigits.union(evenDigits).sort()
+
+oddDigits.intersect(evenDigits).sort()
+
+oddDigits.subtract(singledigitPrimeNumbers).sort()
+
+oddDigits.exclusiveOr(singledigitPrimeNumbers).sort()
+
+// Set Membership and Equality
+let houseAnimals: Set = ["🐶","🐱"]
+let farmAnimals: Set = ["🐮","🐔","🐑","🐶","🐱"]
+let cityAnimals: Set = ["🐦","🐭"]
+
+houseAnimals.isSubsetOf(farmAnimals)
+
+farmAnimals.isSupersetOf(houseAnimals)
+
+farmAnimals.isDisjointWith(cityAnimals)
+
+// Dictionaries
+// Creating an Empty Dictionary
+var namesOfIntegers = [Int: String]()
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers = [:]
+
+// Creating a Dictionary with a Dictionary Literal
+var airports: [String: String] = ["YYZ": "Toronto", "DUB": "Dublin"]
+
+//var airports = ["YYZ": "Toronto", "DUB": "Dublin"]
+
+// Accessing and Modifuing a Dictionary
+print("The airports dictionary contains \(airports.count) items.")
+
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+} else {
+    print("The airports dictionary is not empty.")
+}
+
+airports["LHR"] = "London"
+
+airports["LHR"] = "London Heathrow"
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName).")
+} else {
+    print("That airport is not in the airport dictionay.")
+}
+
+airports["APL"] = "Apple International"
+airports["APL"] = nil
+
+if let removedValue = airports.removeValueForKey("DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain a value for DUB.")
+}
+
+// Iterating Over a Dictionary
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    print("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name: \(airportName)")
+}
+
+let airportCodes = [String](airports.keys)
+let airportNames = [String](airports.values)
+
+
+
+
+
+
+
+
+
+
+
 
 

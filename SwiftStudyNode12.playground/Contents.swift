6557c9231b4bedacf9ab63801c11ccb36e3d7d0a
yanglei <:39:39yanglei3kyou@126.com>
2015-11-02 20:10:33 +0800

    151102B
    Add something knowledge about Closures.

--- SwiftStudyNode12.playground/Contents.swift
+++ SwiftStudyNode12.playground/Contents.swift
@@ -4,3 +4,50 @@ import UIKit
 
 // The Swift Programming Language(Swift 2)
 // Closures
+// The Sort Method
+let names = ["Chris", "Ales", "Ewa", "Barry", "Daniella"]
+
+func backwards(s1: String, _ s2: String)->Bool {
+    return s1 > s2
+}
+
+var reversed = names.sort(backwards)
+
+// Closure Expression Syntax
+reversed = names.sort({
+    (s1: String, s2: String)->Bool in
+    return s1>s2
+})
+
+reversed = names.sort({(s1: String, s2: String)->Bool in return s1>s2 })
+
+// Inferring Type From Context
+reversed = names.sort({s1,s2 in return s1<s2})
+
+// Implicit Returns from Single-Expression Closures
+reversed = names.sort({s1,s2 in s1>s2})
+
+// Shorthand Argument Names
+reversed = names.sort({$0 > $1})
+
+// Operator Functions
+reversed = names.sort(>)
+
+// Trailing Closures
+func someFunctionThatTakesAClosure(closure:()->Void) {
+    
+}
+
+someFunctionThatTakesAClosure({
+    
+})
+
+someFunctionThatTakesAClosure() {
+    
+}
+
+reversed = names.sort() {$0>$1}
+
+reversed = names.sort {$0 > $1}
+
+

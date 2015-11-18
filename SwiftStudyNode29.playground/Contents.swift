1746aef927ac0df3092b8bcc0ed5cc1a2a4bb764
yanglei3kyou <yanglei3kyou@126.com>
2015-11-18 16:33:59 +0800

    151118B
    Add some knowledge about Advanced Operators

--- SwiftStudyNode29.playground/Contents.swift
+++ SwiftStudyNode29.playground/Contents.swift
@@ -2,3 +2,36 @@
 
 // The Swift Programming Language(Swift 2)
 // Access Control
+// Modules and Source Files
+// Access Levels
+/*
+    Public
+    Internal
+    Private
+*/
+
+// Access Control Syntax
+public class SomePublicClass {}
+internal class SomeInternalClass {}
+private class SomePrivateClass {}
+
+public var somePublicVariable = 0
+internal let someInternalConstant = 0
+private func somePrivateFunction() {}
+
+// Getters and Setters
+struct TrackedString {
+    private(set) var numberOfEdits = 0
+    var value: String = "" {
+        didSet {
+            numberOfEdits++
+        }
+    }
+}
+
+var stringToEdit = TrackedString()
+stringToEdit.value = "ddd"
+stringToEdit.value = "ddd"
+stringToEdit.value = "ddd"
+print(stringToEdit.numberOfEdits)
+
